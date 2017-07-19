c0562807b9533152cd9b66b8ce1518004a8efa39
Yuri carranza <alwaysyurixd@hotmail.com>
2017-07-18 22:52:02 -0500

    thrid commit

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-var http = require('http');
+var express = require("express");
+var mysql = require("mysql");
+var bodyParser = require("body-parser");
 
-var server = http.createServer(function(request, response) {
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'bdcs'
 });
 
-var port = process.env.PORT || 1337;
-server.listen(port);
+var app = express();
 
-console.log("Server running at http://localhost:%d", port);
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
+
+var GCM = require('./gcm.js');
+var gcm = new GCM('AIzaSyCQYD3Npron6xO4PiaseIjKcGo1Mje9IXw');
+global.gcm = gcm;
+
